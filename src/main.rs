38c8ddcd3302a87f03a52720f850d96b70c09d49
chlorodose <chlorodose@chlorodose.me>
2025-11-04 21:24:30 +0800

    refactor: migrate oneshot to daemon

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,45 @@ use std::{
     fs::read_to_string,
     net::{IpAddr, Ipv4Addr, Ipv6Addr},
     path::{Path, PathBuf},
-    process::exit,
+    pin::pin,
     str::FromStr,
-    sync::atomic::{AtomicU64, Ordering},
+    sync::{
+        Arc,
+        atomic::{AtomicBool, Ordering},
+    },
+    task::{Context, Waker},
+    time::Duration,
 };
 
 use cidr::{AnyIpCidr, Ipv4Cidr, Ipv6Cidr};
-use futures::StreamExt;
+use futures::{FutureExt, StreamExt};
 use rtnetlink::{
-    RouteHandle, RouteMessageBuilder,
-    packet_route::route::{RouteAddress, RouteAttribute, RouteMessage},
+    MulticastGroup, RouteHandle, RouteMessageBuilder, new_multicast_connection,
+    packet_core::NetlinkPayload,
+    packet_route::{
+        RouteNetlinkMessage,
+        route::{RouteAddress, RouteAttribute, RouteMessage},
+    },
 };
-
-static DONE_NUM: AtomicU64 = AtomicU64::new(0);
-static ERR_NUM: AtomicU64 = AtomicU64::new(0);
+use systemd::daemon::{STATE_READY, STATE_RELOADING, STATE_STATUS, notify as systemd_notify};
+use tokio::{
+    sync::Notify,
+    time::{Instant, sleep},
+};
 
 type GeoItem = (Vec<Ipv4Cidr>, Vec<Ipv6Cidr>);
 
+static IN_ERR: AtomicBool = AtomicBool::new(false);
+
 #[tokio::main]
 async fn main() {
-    let (task, handle, _) = rtnetlink::new_connection().expect("failed to connect to netlink");
+    assert!(
+        systemd_notify(false, [(STATE_STATUS, "initializing..."),].iter(),).unwrap(),
+        "notify systemd failed"
+    );
+    let (task, handle, mut chan) =
+        new_multicast_connection(&[MulticastGroup::Ipv4Route, MulticastGroup::Ipv6Route])
+            .expect("failed to connect to netlink");
     tokio::spawn(task);
     let handle = handle.route();
     let geoip_path = env::var_os("GEOIP_PATH")
@@ -45,67 +64,156 @@ async fn main() {
     let to_table: u8 = str::parse(&env::var("TO_TABLE").unwrap_or("254".to_string()))
         .expect("TO_TABLE is not an valid table num");
 
+    let notify = Arc::new(Notify::new());
+    eprintln!("Starting update thread");
+    tokio::spawn(update_thread(
+        Arc::clone(&notify),
+        handle,
+        from_table,
+        to_table,
+        map,
+    ));
+
+    loop {
+        let msg = chan.next().await.unwrap();
+        if !matches!(
+            msg.0.payload,
+            NetlinkPayload::InnerMessage(
+                RouteNetlinkMessage::DelRoute(_) | RouteNetlinkMessage::NewRoute(_),
+            )
+        ) {
+            continue;
+        }
+        notify.notify_one();
+    }
+}
+async fn update_thread(
+    notify: Arc<Notify>,
+    handle: RouteHandle,
+    from_table: u8,
+    to_table: u8,
+    map: &'static HashMap<IpAddr, GeoItem>,
+) {
+    loop {
+        update(handle.clone(), from_table, to_table, map).await;
+        assert!(
+            systemd_notify(
+                false,
+                [
+                    (STATE_READY, "1"),
+                    (STATE_STATUS, "done, waiting for network changes"),
+                ]
+                .iter(),
+            )
+            .unwrap(),
+            "notify systemd failed"
+        );
+        notify.notified().await;
+        assert!(
+            systemd_notify(
+                false,
+                [
+                    (STATE_RELOADING, "1"),
+                    (STATE_STATUS, "reloading due to network changes"),
+                ]
+                .iter(),
+            )
+            .unwrap(),
+            "notify systemd failed"
+        );
+        loop {
+            eprintln!("Recv network changes, waiting 100ms for more changes to come...");
+            sleep(Duration::from_millis(100)).await;
+            if pin!(notify.notified())
+                .poll_unpin(&mut Context::from_waker(Waker::noop()))
+                .is_pending()
+            {
+                break;
+            }
+        }
+    }
+}
+async fn update(
+    handle: RouteHandle,
+    from_table: u8,
+    to_table: u8,
+    map: &'static HashMap<IpAddr, GeoItem>,
+) {
+    let start_time = Instant::now();
     eprintln!("Flushing routes");
-    let stream = futures::stream::select(
-        handle
-            .get(RouteMessageBuilder::<Ipv4Addr>::new().build())
-            .execute(),
-        handle
-            .get(RouteMessageBuilder::<Ipv6Addr>::new().build())
-            .execute(),
-    );
-    stream
-        .for_each_concurrent(None, async |r| {
-            let Ok(m) = r else {
-                return;
-            };
-            if m.header.table != to_table {
-                return;
-            }
-            let result = handle.del(m).execute().await;
-            if let Err(err) = result {
-                eprintln!("Error: {err}");
-            }
-        })
-        .await;
+    IN_ERR.store(false, Ordering::Relaxed);
+    loop {
+        let stream = futures::stream::select(
+            handle
+                .get(RouteMessageBuilder::<Ipv4Addr>::new().build())
+                .execute(),
+            handle
+                .get(RouteMessageBuilder::<Ipv6Addr>::new().build())
+                .execute(),
+        );
+        stream
+            .for_each_concurrent(None, async |r| {
+                let Ok(m) = r else {
+                    return;
+                };
+                if m.header.table != to_table {
+                    return;
+                }
+                let result = handle.del(m).execute().await;
+                if let Err(err) = result {
+                    eprintln!("Error: {err}");
+                }
+            })
+            .await;
+        if !IN_ERR.load(Ordering::Relaxed) {
+            break;
+        }
+    }
 
     eprintln!("Translating Routes");
-    let stream = futures::stream::select(
-        handle
-            .get(RouteMessageBuilder::<Ipv4Addr>::new().build())
-            .execute(),
-        handle
-            .get(RouteMessageBuilder::<Ipv6Addr>::new().build())
-            .execute(),
-    );
-    stream
-        .for_each_concurrent(None, async |r| {
-            let Ok(mut m) = r else {
-                return;
-            };
-            if m.header.table != from_table {
-                return;
-            }
-            m.header.table = to_table;
-            m.attributes = m
-                .attributes
-                .iter()
-                .filter(|e| !matches!(e, RouteAttribute::Table(_)))
-                .cloned()
-                .collect();
-            m.attributes
-                .push(RouteAttribute::Table(u32::from(to_table)));
-            tokio::spawn(try_map(map, handle.clone(), m)).await.unwrap();
-        })
-        .await;
+    IN_ERR.store(false, Ordering::Relaxed);
+    let sum = loop {
+        let stream = futures::stream::select(
+            handle
+                .get(RouteMessageBuilder::<Ipv4Addr>::new().build())
+                .execute(),
+            handle
+                .get(RouteMessageBuilder::<Ipv6Addr>::new().build())
+                .execute(),
+        );
+        let stream: Vec<_> = stream
+            .filter_map(async |r| {
+                let Ok(mut m) = r else {
+                    return None;
+                };
+                if m.header.table != from_table {
+                    return None;
+                }
+                m.header.table = to_table;
+                m.attributes = m
+                    .attributes
+                    .iter()
+                    .filter(|e| !matches!(e, RouteAttribute::Table(_)))
+                    .cloned()
+                    .collect();
+                m.attributes
+                    .push(RouteAttribute::Table(u32::from(to_table)));
+                Some(tokio::spawn(try_map(map, handle.clone(), m)))
+            })
+            .collect()
+            .await;
+        let mut sum = 0;
+        for task in stream {
+            sum += task.await.unwrap();
+        }
+        if !IN_ERR.load(Ordering::Relaxed) {
+            break sum;
+        }
+    };
     eprintln!(
-        "Done, added {} routes, {} failed",
-        DONE_NUM.load(Ordering::Relaxed),
-        ERR_NUM.load(Ordering::Relaxed)
+        "Done, added {sum} routes, tooks {}ms",
+        Instant::now().duration_since(start_time).as_millis()
     );
-    if ERR_NUM.load(Ordering::Relaxed) != 0 {
-        exit(1);
-    }
 }
 fn read_geoip(name: &str, geoip_path: impl AsRef<Path>) -> GeoItem {
     let mut v4 = Vec::new();
@@ -129,7 +237,11 @@ fn read_geoip(name: &str, geoip_path: impl AsRef<Path>) -> GeoItem {
     });
     (v4, v6)
 }
-async fn try_map(map: &HashMap<IpAddr, GeoItem>, handle: RouteHandle, source: RouteMessage) {
+async fn try_map(
+    map: &HashMap<IpAddr, GeoItem>,
+    handle: RouteHandle,
+    source: RouteMessage,
+) -> usize {
     let mut tasks = Vec::new();
     source.attributes.iter().enumerate().find(|(i, e)| match e {
         RouteAttribute::Destination(RouteAddress::Inet(addr)) => {
@@ -160,14 +272,13 @@ async fn try_map(map: &HashMap<IpAddr, GeoItem>, handle: RouteHandle, source: Ro
         }
         _ => false,
     });
+    let len = tasks.len();
     for h in tasks {
         let result = h.await.unwrap();
         if let Err(err) = result {
             eprintln!("Error: {err}");
-            &ERR_NUM
-        } else {
-            &DONE_NUM
+            IN_ERR.store(true, Ordering::Relaxed);
         }
-        .fetch_add(1, Ordering::Relaxed);
     }
+    len
 }
